f12b7af77f085cab7af4c5e1fe4e61db8641f8d0
Rudra Chakraborty <chakraborty.rudra.03@gmail.com>
2018-07-17 15:23:51 -0400

    Cloud Setup main class

---
+++ classes/OpenXdmod/Setup/CloudSetup.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace OpenXdmod\Setup;
+
+class CloudSetup extends SubMenuSetupItem
+{
+    /**
+     * This setup menu.
+     *
+     * @var Menu
+     */
+    protected $menu;
+
+    /**
+     * True if setup should quit.
+     *
+     * @var bool
+     */
+    protected $quit;
+
+    /**
+     * @inheritdoc
+     */
+    public function __construct(Console $console)
+    {
+        parent::__construct($console);
+
+        $items = array(
+            new MenuItem('q', 'Quit Cloud Metrics Setup', new SubMenuQuitSetup($console, $this)),
+        );
+
+        $this->menu = new Menu($items, $this->console, 'Cloud Metrics Setup');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function handle()
+    {
+        $this->quit = false;
+
+        while (!$this->quit) {
+            $this->menu->display();
+        }
+    }
+
+    /**
+     * Call to exit the menu on the next cycle.
+     */
+    public function quit()
+    {
+        $this->quit = true;
+    }
+
+    /**
+     * No options to save data for this submenu
+     */
+    public function save()
+    {
+    }
+}
