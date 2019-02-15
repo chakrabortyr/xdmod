5e50e441eb668b8b3d03c4f42b435047a15de7ac
Rudra Chakraborty <chakraborty.rudra.03@gmail.com>
2019-02-15 13:47:13 -0500

    things

--- classes/ETL/Ingestor/CloudStateReconstructorTransformIngestor.php
+++ classes/ETL/Ingestor/CloudStateReconstructorTransformIngestor.php
@@ -132,10 +132,10 @@ class CloudStateReconstructorTransformIngestor extends pdoIngestor implements iA
         } elseif (in_array($srcRecord['event_type_id'], $this->_stop_event_ids) && !$this->foundFirstStop()) {
             $this->updateInstance($srcRecord);
         } elseif (in_array($srcRecord['event_type_id'], $this->_stop_event_ids) && $this->foundFirstStop()) {
-            $oldDateTime = $this->_instance_state['end_time'];
-            $newDateTime = $srcRecord['event_time_utc'];
+            $oldDateTime = strtotime($this->_instance_state['end_time']);
+            $newDateTime = strtotime($srcRecord['event_time_utc']);
 
-            if ($newDateTime <= $oldDateTime) {
+            if ($newDateTime < $oldDateTime) {
                 $this->updateInstance($srcRecord);
                 $transformedRecord[] = $this->_instance_state;
                 $this->resetInstance();
